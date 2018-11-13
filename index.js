b371f4d446f5a36d8b5a04387c783c96b0d58654
nudelx <nudelx@gmail.com>
2018-11-13 14:26:49 +0200

    after effect

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function(defaultValue, debounce) {
+export default function(defaultValue, debounce, afterEffect) {
   const [value, onInputChange] = React.useState(defaultValue)
 
   React.useEffect(
     () => {
-      const timer = setTimeout(() => onInputChange(value), debounce || 400)
+      const timer = setTimeout(() => {
+        onInputChange(value)
+        typeof afterEffect === 'function' && afterEffect()
+      }, debounce || 400)
       return () => clearTimeout(timer)
     },
     [value]
